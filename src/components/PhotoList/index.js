6ee8830b0fbd8caa23d4cbdb8a59c39d75f30ced
kdingman <kcdonlan@yahoo.com>
2022-04-13 18:52:31 -0400

    modal component set up

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 
-//import Modal from '../Modal';
+import Modal from '../Modal';
 
 function PhotoList({ category }) {
+
+  const [isModalOpen, setIsModalOpen] = useState(false); // false -> we don't want the modal to open unless image is clicked
+
+  const [currentPhoto, setCurrentPhoto] = useState();
 
     const [photos] = useState([
         {
@@ -105,14 +109,22 @@ function PhotoList({ category }) {
       // FilterMethod
       const currentPhotos = photos.filter((photo) => photo.category === category);
 
+      const toggleModal = (image, i) => {
+        setCurrentPhoto({ ...image, index: i })
+
+        setIsModalOpen(true);
+      }
+
     return (
         <div>
+          {isModalOpen && <Modal currentPhoto={currentPhoto} />} 
             <div className='flex-row'>
                 {currentPhotos.map((image, i) => (
                     <img
                         src={require(`../../assets/small/${category}/${i}.jpg`)}
                         alt={image.name}
                         className='img-thumbnail mx-1'
+                        onClick={() => toggleModal(image, i)}
                         key={image.name}
                     />
                     ))}
